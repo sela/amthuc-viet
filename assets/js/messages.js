9c87c8904602fbaa290b1ba775e45ccdbc1d9851
Sela Yair <sela@sela-v.com>
2021-05-14 22:19:21 +0100

    update closing notice

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,12 +1,13 @@
-//jQuery(document).ready(function($) {
-//
-//    var xmlhttp = new XMLHttpRequest();
-//    xmlhttp.onreadystatechange = function () {
-//        if (this.readyState == 4 && this.status == 200) {
-//            var myArr = JSON.parse(this.responseText);
-//            document.getElementById("amthucModalMessage").innerHTML = myArr[0]["title"];
-//        }
-//    };
-//    xmlhttp.open("GET", {% site.data.messages | jsonify %}, true);
-//    xmlhttp.send();
-//});
+jQuery(document).ready(function($) {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            //document.getElementById("amthucModalMessage").innerHTML = myArr[0]["title"];
+            $('#amthucModalMessage .modal-body').innerHTML = 'test SELA';
+        }
+    };
+    //xmlhttp.open("GET", 'assets/messages.json', true);
+    //xmlhttp.send();
+});
