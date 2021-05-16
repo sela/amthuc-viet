978ac1eebb706207e321fc9be81de99397bab296
Sela Yair <sela@sela-v.com>
2021-05-16 17:53:48 +0100

    messages enabled

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,13 +1,18 @@
 jQuery(document).ready(function($) {
-
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
-            //document.getElementById("amthucModalMessage").innerHTML = myArr[0]["title"];
-            $('#amthucModalMessage .modal-body').innerHTML = 'test SELA';
+            var data = JSON.parse(this.responseText);
+            if (typeof data[0] !== 'undefined') {
+                if ($.cookie('modal_shown') == null) {
+                    $('#amthucModalMessage .modal-title').text(data[0]["title"]);
+                    $('#amthucModalMessage .modal-body').text(data[0]["body"]);
+                    $.cookie('modal_shown', 'yes', {expires: 1, path: '/'});
+                    $('#amthucModalMessage').modal('show');
+                }
+            }
         }
     };
-    //xmlhttp.open("GET", 'assets/messages.json', true);
-    //xmlhttp.send();
+    xmlhttp.open("GET", '/assets/messages.json', true);
+    xmlhttp.send();
 });
